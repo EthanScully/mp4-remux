f36bc92a85c1fbb938a3ba69c156b0a8f5241182
EthanScully <89865501+EthanScully@users.noreply.github.com>
2024-06-04 03:05:04 -0400

    refactor file.go

--- cli/file/file.go
+++ cli/file/file.go
@@ -6,55 +6,30 @@ import (
 	"strings"
 )
 
-func fileCheck(name string, or ...int) (final string, err error) {
-	var num int
-	if len(or) == 0 {
-		num = 0
-	} else {
-		num = or[0] + 1
-	}
-	files, err := os.ReadDir(".")
-	if err != nil {
-		panic(err)
-	}
-	for _, file := range files {
-		if num == 0 {
-			if file.Name() == fmt.Sprintf("%s.mp4", name) {
-				final, err = fileCheck(name, num)
-				return
-			}
-		} else {
-			if file.Name() == fmt.Sprintf("%s(%d).mp4", name, num) {
-				final, err = fileCheck(name, num)
-				return
+func fileCheck(name string) (final string) {
+	final = name + ".mp4"
+	if _, err := os.Stat(final); err == nil {
+		for i := 1; i > 0; i++ {
+			new := fmt.Sprintf("%s(%d)", name, i)
+			if _, err := os.Stat(new + ".mp4"); err != nil {
+				err = nil
+				final = new + ".mp4"
+				break
 			}
 		}
-	}
-	if num == 0 {
-		final = name + ".mp4"
-	} else {
-		final = fmt.Sprintf("%s(%d).mp4", name, num)
 	}
 	return
 }
 
-// Determines ouput filename with .mp4 attached, if file exists, it intelligently returns new name
+// Determines ouput filename with .mp4 attached, if file exists, it returns new name
 func ParseName(path string) (filename string, err error) {
-	if len(path) < 5 {
-		err = fmt.Errorf("path length")
-		return
+	filename = path
+	if i := strings.LastIndex(path, string(os.PathListSeparator)); i != -1 {
+		filename = path[i+1:]
 	}
-	if !strings.Contains(path[len(path)-5:], ".") {
-		err = fmt.Errorf("path must contain a file extension")
-		return
+	if i := strings.LastIndex(path, "."); i != -1 {
+		filename = path[:i]
 	}
-	extLoc := strings.LastIndex(path, ".")
-	if !strings.Contains(path, string(os.PathSeparator)) {
-		filename = path[:extLoc]
-	} else {
-		lastSep := strings.LastIndex(path, string(os.PathSeparator))
-		filename = path[lastSep+1 : extLoc]
-	}
-	filename, err = fileCheck(filename)
+	filename = fileCheck(filename)
 	return
 }
