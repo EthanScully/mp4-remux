356687e0d303f537b29eeb6989b5c1baaad79f40
EthanScully <89865501+EthanScully@users.noreply.github.com>
2024-09-06 16:39:23 -0400

    added batch processing

--- cli/main.go
+++ cli/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/EthanScully/mp4-remux/cli/file"
 	ffmpeg "github.com/EthanScully/mp4-remux/lib"
@@ -18,10 +21,54 @@ func main() {
 		fmt.Printf("Usage: %s <video-path>\n", str)
 		return
 	}
-	path := os.Args[1]
+	if os.Args[1][len(os.Args[1])-1] == '*' {
+		batch(os.Args[1][:len(os.Args[1])-1])
+		return
+	} else {
+		run(os.Args[1])
+		return
+	}
+}
+func batch(directory string) {
+	prefix := directory
+	directory = directory[:len(directory)-1]
+	dir, err := os.ReadDir(directory)
+	if err != nil {
+		fmt.Printf("error reading the directory: %s\n", directory)
+	}
+	var todo []string
+	for _, file := range dir {
+		if !file.IsDir() {
+			filepath := prefix + file.Name()
+			todo = append(todo, filepath)
+		}
+	}
+	var wg sync.WaitGroup
+	var mtx sync.Mutex
+	var running int
+	threads := runtime.NumCPU()
+	for _, path := range todo {
+		wg.Add(1)
+		go func(path string) {
+			defer wg.Done()
+			mtx.Lock()
+			running++
+			mtx.Unlock()
+			run(path)
+			mtx.Lock()
+			running--
+			mtx.Unlock()
+		}(path)
+		for running >= threads {
+			time.Sleep(time.Microsecond * 250)
+		}
+	}
+	wg.Wait()
+}
+func run(path string) {
 	filename := file.ParseName(path)
 	err := ffmpeg.Remux(path, filename)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 }
